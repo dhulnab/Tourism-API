810c2d640d23adc33c0a7bd020c58777cf2cd14a
Dhulfiqar Ali <dhulfiqarali6@gmail.com>
2024-01-30 09:33:43 +0300

    fix some bugs

--- routers/trips.js
+++ routers/trips.js
@@ -5,14 +5,15 @@ const {
   getTrip,
   deleteTrip,
 } = require("../models/trip");
+const { checkAuthCompany } = require("../middleware");
 const express = require("express");
 const router = express.Router();
 
 //Trips table
 router.get("/trip/view", trips);
 router.get("/trip/get/:id", getTrip);
-router.post("/trip/add/:id", addTrip);
-router.put("/trip/edit/:id", editTrip);
-router.delete("/trip/delete/:id", deleteTrip);
+router.post("/trip/add/:id", checkAuthCompany, addTrip);
+router.put("/trip/edit/:id", checkAuthCompany, editTrip);
+router.delete("/trip/delete/:id", checkAuthCompany, deleteTrip);
 
 module.exports = router;
